6361f140b89cf611e84fe6d90fab4f8c20d3bac2
santiago_toro <storomes@gmail.com>
2025-06-09 13:31:36 -0500

    commit changes

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnswitch = document.querySelector('#switch');
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
     btnswitch.addEventListener('click', () => {
         document.body.classList.toggle('dark');
         btnswitch.classList.toggle('active');
+    });
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            nav.classList.remove('active');
+        });
     });
 });
